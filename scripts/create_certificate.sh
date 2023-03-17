b5dbe8ca04544d755816a53ae7f522aa05e0d0bd
lhaj-matich <choumouloussama@gmail.com>
2023-03-17 18:16:14 +0100

    Modified the script for SSL certificate generation.

--- scripts/create_certificate.sh
+++ scripts/create_certificate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -sha384 -keyout server-cert.key -out server-cert.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server-cert.key -out server-cert.crt
+
