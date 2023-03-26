17344aa50f8334e35079c18eefb7a89243230123
lhaj-matich <choumouloussama@gmail.com>
2023-03-26 12:49:39 +0000

    Added the sleep command to stop execution for a few seconds.

--- cloud/src/requirements/mariadb/tools/docker_entry.sh
+++ cloud/src/requirements/mariadb/tools/docker_entry.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+SLEEP_TIME="3"
+
 # Allow execute permissions to these scripts
 chmod +x db_setup.sh
 chmod +x wp_setup.sh
-# Setup the databases for wordpress and mariadb and Stop mariadb service
-./db_setup.sh && ./wp_setup.sh && service mariadb stop
+# Setup the databases for wordpress and mariadb
+./db_setup.sh && ./wp_setup.sh
+# Stop the execution for a few seconds
+sleep ${SLEEP_TIME}
+# Stop mariadb service
+service mariadb stop
 # Start mariadb server in the foreground
 mariadbd --bind-address=0.0.0.0
