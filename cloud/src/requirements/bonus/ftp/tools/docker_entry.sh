a543f5cf667a00189d69c3f3ce010c5f80d356f2
lhaj-matich <choumouloussama@gmail.com>
2023-04-14 21:43:48 +0000

    Still teaking the ftp.

--- cloud/src/requirements/bonus/ftp/tools/docker_entry.sh
+++ cloud/src/requirements/bonus/ftp/tools/docker_entry.sh
@@ -7,10 +7,12 @@ export FTP_PASS="biden_1234"
 # Add the user that will be responsible for logging in
 
 if [ ! -e /etc/vsftp_status ]; then
-    useradd -G $FTP_USER -m -p $(openssl passwd -1 $FTP_PASS) $FTP_USER
+    useradd -m -p $(openssl passwd -1 $FTP_PASS) $FTP_USER
     usermod -d /var/www/html $FTP_USER
     echo $FTP_USER > /etc/vsftpd.chroot_list
     chown $FTP_USER:$FTP_USER /var/www/html/
+    mkdir -p /var/run/vsftpd
+    chmod 755 /var/run/vsftpd
     chmod 755 /var/www/html/
     touch /etc/vsftp_status
 fi
