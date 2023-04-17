84aedf101d54b8d90ffa952341f6ea29ce9348ab
lhaj-matich <choumouloussama@gmail.com>
2023-04-17 04:41:32 +0000

    Trying to configure redis am having some protection mode issues.

--- cloud/src/requirements/wordpress/tools/docker_entry.sh
+++ cloud/src/requirements/wordpress/tools/docker_entry.sh
@@ -11,22 +11,28 @@ export MARIADB_DBNAME="wordpress"
 mkdir -p /run/php
 sed -i "s/\/run\/php\/php7.4-fpm.sock/0.0.0.0:9000/g" /etc/php/7.4/fpm/pool.d/www.conf
 
-# should run the download of wordpress inside the image so that wordpress setup will run much faster
+#! maybe i should change the file that am testing for i mean latest.tar.gz
 if test -f "latest.tar.gz"; then
-tar xfz latest.tar.gz
-mkdir -p /var/www/html/wordpress/
-cp -r wordpress/* /var/www/html/wordpress/
+    tar xfz latest.tar.gz
+    mkdir -p /var/www/html/wordpress/
+    cp -r wordpress/* /var/www/html/wordpress/
+    cp wp-config-sample.php /var/www/html/wordpress/
 
-sed -i "s/database_name_here/$MARIADB_DBNAME/g" /var/www/html/wordpress/wp-config-sample.php
-sed -i "s/username_here/$MARIADB_USER/g" /var/www/html/wordpress/wp-config-sample.php
-sed -i "s/password_here/$MARIADB_PASS/g" /var/www/html/wordpress/wp-config-sample.php
-sed -i "s/localhost/$MARIADB_HOST/g" /var/www/html/wordpress/wp-config-sample.php
+    sed -i "s/database_name_here/$MARIADB_DBNAME/g" /var/www/html/wordpress/wp-config-sample.php
+    sed -i "s/username_here/$MARIADB_USER/g" /var/www/html/wordpress/wp-config-sample.php
+    sed -i "s/password_here/$MARIADB_PASS/g" /var/www/html/wordpress/wp-config-sample.php
+    sed -i "s/localhost/$MARIADB_HOST/g" /var/www/html/wordpress/wp-config-sample.php
 
-#DEBUG: check that the variables are getting replaced by sed.
-cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+    #! Further check here not to download and extract the plugin twice.
+    echo "Downloading redis plugin."
+    wget https://downloads.wordpress.org/plugin/redis-cache.latest-stable.zip
+    echo "Extracting plugin."
+    unzip -q redis-cache.latest-stable.zip -d /var/www/html/wordpress/wp-content/plugins/
+    #DEBUG: check that the variables are getting replaced by sed.
+    cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 else
     echo "Downloading wordpress failed."
 fi
 
-echo "Wordpress is listenning on port 9001"
+echo "Wordpress is listenning on port 9000"
 exec php-fpm7.4 -F -R
