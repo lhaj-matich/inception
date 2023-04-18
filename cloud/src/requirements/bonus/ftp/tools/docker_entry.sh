e990003d46ae078bf1278651d2bd4f5b3e60aa21
lhaj-matich <choumouloussama@gmail.com>
2023-04-18 15:01:45 +0000

    starting to set the static page, setting up the reverse proxy.

--- cloud/src/requirements/bonus/ftp/tools/docker_entry.sh
+++ cloud/src/requirements/bonus/ftp/tools/docker_entry.sh
@@ -10,10 +10,10 @@ if [ ! -e /etc/vsftp_status ]; then
     useradd -m -p $(openssl passwd -1 $FTP_PASS) $FTP_USER
     usermod -d /var/www/html $FTP_USER
     echo $FTP_USER > /etc/vsftpd.chroot_list
-    chown $FTP_USER:$FTP_USER /var/www/html/
+    chown $FTP_USER:$FTP_USER /var/www/html/wordpress
     mkdir -p /var/run/vsftpd
     chmod 755 /var/run/vsftpd
-    chmod 755 /var/www/html/
+    chmod 755 /var/www/html/wordpress
     touch /etc/vsftp_status
 fi
 
