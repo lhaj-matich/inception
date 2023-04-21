b70625de5907875b00479c35e8aca4cef5277ba2
lhaj-matich <choumouloussama@gmail.com>
2023-04-21 04:15:50 +0000

    I fixed the ftp stopping issue.

--- cloud/src/requirements/bonus/ftp/tools/docker_entry.sh
+++ cloud/src/requirements/bonus/ftp/tools/docker_entry.sh
@@ -9,7 +9,7 @@ export FTP_PASS="biden_1234"
 if [ ! -e /etc/vsftp_status ]; then
     useradd -m -p $(openssl passwd -1 $FTP_PASS) $FTP_USER
     usermod -d /var/www/html/wordpress $FTP_USER
-    echo $FTP_USER > /etc/vsftpd.chroot_list
+    echo $FTP_USER >> /etc/vsftpd.userlist
     chown -R $FTP_USER:$FTP_USER /var/www/html/wordpress
     mkdir -p /var/run/vsftpd
     chmod 755 /var/run/vsftpd
@@ -17,4 +17,7 @@ if [ ! -e /etc/vsftp_status ]; then
     touch /etc/vsftp_status
 fi
 
+# exec vsftpd
+/usr/sbin/vsftpd
+echo "FTP is listenning on port 21"
 # Start the vsftp server in the forground
