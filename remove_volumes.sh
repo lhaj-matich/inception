996e7558d76eeb72766c7cd0ad660d63e28451af
lhaj-matich <choumouloussama@gmail.com>
2023-05-10 07:52:55 +0100

    Small modification in the volume script.

--- remove_volumes.sh
+++ remove_volumes.sh
@@ -2,4 +2,4 @@
 
 sudo rm -rf /home/sn4r7/docker/sql/*
 sudo rm -rf /home/sn4r7/docker/wordpress/*
-sudo docker volume rm src_wpdata src_mariadb
+sudo docker volume rm src_wpdata src_mariadb inception_wpdata inception_mariadb
